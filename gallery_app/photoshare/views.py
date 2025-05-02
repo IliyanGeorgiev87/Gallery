1f6ab7dc4eaf5e5f31f44f278280c1a7352c7f9f
IliyanGeorgiev87 <iliyangeorgiev28@itpg-varna.bg>
2025-05-02 17:05:51 +0300

    styling, category filtering

--- gallery_app/photoshare/views.py
+++ gallery_app/photoshare/views.py
@@ -4,9 +4,15 @@ from .models import Category, Photo
 # Create your views here.
 
 def gallery(request):
+    category = request.GET.get('category')
+
+    if category == None:
+        photos = Photo.objects.all()
+    else:
+        photos = Photo.objects.filter(category__name__contains = category)
+
     categories = Category.objects.all()
     photos = Photo.objects.all()
-
     context = {'categories': categories, 'photos': photos}
     
     return render(request, 'photoshare/gallery.html', context)

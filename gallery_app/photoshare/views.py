f094ba2561c5f9a68e77e8a4e8f81aeafb4a1a4b
IliyanGeorgiev87 <iliyangeorgiev28@itpg-varna.bg>
2025-05-02 17:08:38 +0300

    filtering fix

--- gallery_app/photoshare/views.py
+++ gallery_app/photoshare/views.py
@@ -4,15 +4,15 @@ from .models import Category, Photo
 # Create your views here.
 
 def gallery(request):
+    
     category = request.GET.get('category')
 
     if category == None:
         photos = Photo.objects.all()
     else:
-        photos = Photo.objects.filter(category__name__contains = category)
+        photos = Photo.objects.filter(category__name=category)
 
     categories = Category.objects.all()
-    photos = Photo.objects.all()
     context = {'categories': categories, 'photos': photos}
     
     return render(request, 'photoshare/gallery.html', context)
